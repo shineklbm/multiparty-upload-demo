2c9a0c981aa7ef426749300c71d821d1f98ba7e5
Shine Sudarsanan <shineklbm@gmail.com>
2016-02-29 17:31:02 +0530

    removed the BodyParser, with multiparty we don't need it anymore

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart({uploadDir : "temp"});
 const fs = require('fs');
@@ -9,15 +9,14 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get('/', function(req, res){
 	res.render("pages/index");
 });
 
 app.post('/', multipartMiddleware, function(req, res){
 	var x = 10; // this is just a demo condition, you need to apply your business logic here
+	console.log(req.body.name);
+	console.log(req.body.address);
 	var extension = req.files.profile_picture.originalFilename.split(".").pop();
 	var filename = Math.random().toString(36).substr(2, 5); //please update this with date and time also, just using as a demo
 	if(x === 10){
@@ -34,8 +33,6 @@ app.post('/', multipartMiddleware, function(req, res){
 
 	}
 });
-
-
 
 app.listen(3000, function () {
   console.log('Server running on 3000!');
